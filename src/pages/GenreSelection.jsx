714dddbc72fdba3b2ee3d8a424173dd24fdf95c7
Anurag Khobragae <anuragk2204@gmail.com>
2024-09-15 23:24:40 +0530

    genre-1

---
+++ src/pages/GenreSelection.jsx
@@ -0,0 +1,14 @@
+const GenreSelection = ()=>{
+    return(
+        <>
+        <div className="h-full flex flex-col justify-center">
+            <div className="w-full flex flex-row justify-center">
+                <img className="h-auto max-w-lg rounded-lg" src="src/images/genreImage/pexels-pixabay-45853.jpg" alt="image description"/>
+
+            </div>
+        </div>
+        </>
+    )
+};
+
+export default GenreSelection;
