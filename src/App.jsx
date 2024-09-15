5d384fbd9309eb3e7c186c22576d319feedbc708
Prashant <prashantiiitnag@gmail.com>
2024-09-15 22:36:30 +0530

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div className='bg-red-500'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-const landingpage = () => {
+const LandingPage = () => {
+
+    const [grpname, setGrpName] = React.useState("");
+
+    const handleChange = (e) => {
+        setGrpName(e.target.value);
+    }
 
     return(
     <div>
-        <div className="">
-            <input placeholder="Group 1 Name" >
-
+        <div className="bg-red-500 flex justify-center m-60">
+            <input placeholder="Group 1 Name" type="text" onChange={handleChange} className="h-12 w-52">
             </input>
         </div>
     </div>
     )    
 }
-export default landingpage;
+export default LandingPage;
