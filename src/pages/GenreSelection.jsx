75abd0e7b217b9516f46849d98eafc494e50492d
Anurag Khobragae <anuragk2204@gmail.com>
2024-09-16 01:05:28 +0530

    genre-2

--- src/pages/GenreSelection.jsx
+++ src/pages/GenreSelection.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react'
+
 const GenreSelection = ()=>{
+    const [genre,setGenre] = useState("")
+
+    
+
     return(
         <>
-        <div className="h-full flex flex-col justify-center">
+        <div className="h-full flex flex-col justify-center	items-center min-h-screen">
             <div className="w-full flex flex-row justify-center">
-                <img className="h-auto max-w-lg rounded-lg" src="src/images/genreImage/pexels-pixabay-45853.jpg" alt="image description"/>
+                <div  onClick={()=>{
+                    setGenre("english")
+                    // console.log("English")
+                }}>
+                    <img className="h-auto max-w-sm rounded-lg m-5     
+                    hover:scale-105 transform transition duration-300 ease-in-out shadow-lg hover:md:shadow-[#62cee9]" 
+                    src="src/images/genreImage/ENGLISH.png" alt="image description"/>
+                </div>
 
+                <div onClick={()=>{
+                    setGenre("hindi")
+                    // console.log("Hindi")
+                }}>
+                    <img className="h-auto max-w-sm rounded-lg m-5 
+                    hover:scale-105 transform transition duration-300 ease-in-out shadow-lg hover:shadow-[#cad776]" src="src/images/genreImage/hindi.png" alt="image description"/>
+                </div>
+                
+                <div onClick={()=>{
+                    setGenre("mix")
+                    // console.log("Mix")
+                    // console.log(genre)
+                }}>
+                    <img className="h-auto max-w-sm rounded-lg m-5    
+                    hover:scale-105 transform transition duration-300 ease-in-out shadow-lg hover:shadow-[#ffc9d9]" src="src/images/genreImage/image.png" alt="image description"/>
+                </div>
             </div>
         </div>
         </>
