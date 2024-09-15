54f3e5055501e9db9224ba7d94bcea11f8d23b96
Prashant <prashantiiitnag@gmail.com>
2024-09-15 21:59:07 +0530

    folders added

---
+++ src/pages/landingpage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const landingpage = () => {
+
+    return(
+    <div>
+        <div className="">
+            <input placeholder="Group 1 Name" >
+
+            </input>
+        </div>
+    </div>
+    )    
+}
+export default landingpage;
