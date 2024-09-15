03eb33cb7c1c04b3a0ed37fe0af4f29e49d9b6b8
Prashant <prashantiiitnag@gmail.com>
2024-09-15 22:47:48 +0530

    minor

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -10,9 +10,11 @@ const LandingPage = () => {
 
     return(
     <div>
-        <div className="bg-red-500 flex justify-center m-60">
-            <input placeholder="Group 1 Name" type="text" onChange={handleChange} className="h-12 w-52">
-            </input>
+        <div>
+        <div class="mb-6">
+        <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default input</label>
+        <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
         </div>
     </div>
     )    
